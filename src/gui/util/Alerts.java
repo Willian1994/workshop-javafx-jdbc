7bce7a80ff304fef365a75361a535a76e3d809fc
Willian <willianbuonaduce@gmail.com>
2021-01-11 21:07:03 -0300

    About view

---
+++ src/gui/util/Alerts.java
@@ -0,0 +1,25 @@
+  
+package gui.util;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+public class Alerts {
+
+	public static void showAlert(String title, String header, String content, AlertType type) {
+		Alert alert = new Alert(type);
+		alert.setTitle(title);
+		alert.setHeaderText(header);
+		alert.setContentText(content);
+		alert.show();
+	}
+}
+
+	public static void setTextFieldDouble(TextField txt) {
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+		    	if (newValue != null && !newValue.matches("\\d*([\\.]\\d*)?")) {
+                    txt.setText(oldValue);
+                }
+		    });
+	}
+}
